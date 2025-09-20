ff49c8ff52a476f852e539389aada45ded905c0e
merakera <meronabera2121@gmail.com>
2025-09-19 22:26:28 -0700

    Project frontend and backend structure jdk 17 and node 20

---
+++ resume-backend/src/main/java/com/resume/resume_backend/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.resume.resume_backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173") // allow your React dev server
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Backend is running!";
+    }
+}
